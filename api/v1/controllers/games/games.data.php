3fde9e427c8d485693b7a46d24e679b010efed56
Rachel Dotey <rachel.dotey@gmail.com>
2016-02-01 12:45:52 -0500

    Created an elaborate game object to be returned for display.

--- api/v1/controllers/games/games.data.php
+++ api/v1/controllers/games/games.data.php
@@ -3,30 +3,74 @@
 
 class GameData {
   
-    public static function getGame($id) {
-        $game = DBConn::selectOne("SELECT r.id, r.game, r.desc, r.created, r.last_updated AS lastUpdated, "
-                . "CONCAT(u1.name_first, ' ', u1.name_last) AS createdBy, "
-                . "CONCAT(u2.name_first, ' ', u2.name_last) AS updatedBy "
-                . "FROM as_auth_games AS r "
-                . "JOIN as_users AS u1 ON u1.id = r.created_user_id "
-                . "JOIN as_users AS u2 ON u2.id = r.last_updated_by WHERE r.id = :id;", array(':id' => $id));
+    static function getGame($id) {
+        $game = DBConn::selectOne("SELECT g.id, g.name, g.scheduled, g.venue_id AS venueId, g.host_user_id AS hostId, "
+                . "game_started AS started, game_ended AS ended, max_points maxPoints, "
+                . "CONCAT(u.name_first, ' ', u.name_last) AS updatedBy "
+                . "FROM as_games AS g "
+                . "JOIN as_users AS u ON u.id = g.last_updated_by WHERE g.id = :game_id LIMIT 1;", array(':game_id' => $id));
 
-        $qFields = DBConn::preparedQuery("SELECT e.id, e.identifier, e.desc "
-                . "FROM as_auth_fields AS e "
-                . "JOIN as_auth_lookup_game_field AS look ON e.id = look.auth_field_id "
-                . "WHERE look.auth_game_id = :id ORDER BY e.identifier;");
-        
-        $qGroups = DBConn::preparedQuery("SELECT g.id, g.group, g.desc "
-                . "FROM as_auth_groups AS g "
-                . "JOIN as_auth_lookup_group_game AS look ON g.id = look.auth_group_id "
-                . "WHERE look.auth_game_id = :id ORDER BY g.group;");
         
         if($game) {
-            $qGroups->execute(array(':id' => $id));
-            $game->groups = $qGroups->fetchAll(\PDO::FETCH_OBJ);
+            // Host
+            $game->host = DBConn::selectOne("SELECT id, name_first AS nameFirst, name_last AS nameLast, email, "
+                    . "CONCAT(name_first, ' ', name_last) AS displayName "
+                    . "FROM as_users WHERE id = :hotst_user_id LIMIT 1;", array(':hotst_user_id' => $game->hostId));
+            unset($game->hostId);
+                        
+            // Venue
+            $game->venue = DBConn::selectOne("SELECT id, name, address, address_b AS addressB, city, state, zip, phone, website "
+                    . "FROM as_venues WHERE id = :venue_id LIMIT 1;", array(':venue_id' => $game->venueId));
+            unset($game->venueId);
+                        
+            // Scoreboard
+            $game->scoreboard = DBConn::selectAll("SELECT s.team_id AS teamId, s.score, s.game_winner AS winner, s.last_updated AS updated, t.name AS team, "
+                    . "CONCAT(u.name_first, ' ', u.name_last) AS updatedBy "
+                    . "FROM `as_game_score_teams` AS s JOIN as_teams AS t ON t.id = s.team_id "
+                    . "JOIN as_users AS u ON u.id = s.last_updated_by WHERE s.game_id = 1 ORDER BY s.score;", array(':game_id' => $id));
             
-            $qFields->execute(array(':id' => $id));
-            $game->elements = $qFields->fetchAll(\PDO::FETCH_OBJ);
+            // Teams and their score
+            $gTeams = DBConn::executeQuery("SELECT t.id, t.name FROM as_teams AS t "
+                    . "JOIN as_game_score_teams AS g ON g.team_id = t.id WHERE g.game_id = :game_id;", array(':game_id' => $id));
+
+            $qMembers = DBConn::preparedQuery("SELECT m.joined, u.id, u.name_first AS nameFirst, u.name_last AS nameLast, "
+                    . "CONCAT(u.name_first, ' ', u.name_last) AS displayName "
+                    . "FROM as_team_members AS m "
+                    . "LEFT JOIN as_users AS u ON u.id = m.user_id "
+                    . "WHERE m.team_id = :team_id;");
+            
+            $teams = Array();
+            while($team = $gTeams->fetch(\PDO::FETCH_OBJ)) {
+                // Members
+                $qMembers->execute(array(':team_id' => $team->id));
+                $team->members = $qMembers->fetchAll(\PDO::FETCH_OBJ);
+                
+                array_push($teams, $team);
+            }
+            $game->teams = $teams;
+            
+            // Game Rounds
+            $qGameRounds = DBConn::executeQuery("SELECT r.id, r.name, r.max_points AS maxPoints, r.last_updated AS updated, "
+                    . "CONCAT(u.name_first, ' ', u.name_last) AS updatedBy "
+                    . "FROM as_game_rounds AS r JOIN as_users AS u ON r.last_updated_by = u.id "
+                    . "WHERE r.game_id = :game_id ORDER BY r.id;", array(':game_id' => $id));
+            
+            $qRoundScores = DBConn::preparedQuery("SELECT s.score, s.last_updated AS updated, t.id AS teamId, t.name AS team, "
+                    . "CONCAT(u.name_first, ' ', u.name_last) AS updatedBy "
+                    . "FROM as_game_score_rounds AS s JOIN as_teams AS t ON t.id = s.team_id "
+                    . "JOIN as_users AS u ON u.id = s.last_updated_by WHERE s.game_round_id = :game_round_id ORDER BY s.score;");
+            
+            $rounds = Array();
+            while($round = $qGameRounds->fetch(\PDO::FETCH_OBJ)) {
+                // Team Round Scores
+                $qRoundScores->execute(array(':game_round_id' => $round->id));
+                $round->scores = $qRoundScores->fetchAll(\PDO::FETCH_OBJ);
+                
+                array_push($rounds, $round);
+            }
+            $game->rounds = $rounds;
+            
+            return $game;
         }
         return $game;
     }
