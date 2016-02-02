7e12333dd9f0ef9e02a9917c9ab63442bb591dd1
Rachel Dotey <rachel.dotey@gmail.com>
2016-02-02 00:00:44 -0500

    Added member and host scoreboard pages.

---
+++ www/app/views/host/scoreboard/scoreboard.controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* 
+ * Host Scoreboard Page
+ * 
+ * Controller the game for the host.
+ */
+
+angular.module('app.host.scoreboard', [])
+    .controller('HostScoreboardDashboardCtrl', ['$scope', function($scope) {
+
+        
+    }]);
---
+++ www/app/views/member/scoreboard/scoreboard.controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* 
+ * Host Scoreboard Page
+ * 
+ * Controller the game for the host.
+ */
+
+angular.module('app.member.scoreboard', [])
+    .controller('MemberScoreboardDashboardCtrl', ['$scope', function($scope) {
+
+        
+    }]);
