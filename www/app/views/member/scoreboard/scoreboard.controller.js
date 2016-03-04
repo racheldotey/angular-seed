cd36d47eab7e36a3e808d9f4f03211bfdca319e8
Rachel Dotey <rachel.dotey@gmail.com>
2016-03-03 21:00:30 -0500

    Updates.

--- www/app/views/member/scoreboard/scoreboard.controller.js
+++ www/app/views/member/scoreboard/scoreboard.controller.js
@@ -11,23 +11,12 @@ angular.module('app.member.scoreboard', [])
         function($scope, currentGame, DTOptionsBuilder, DTColumnDefBuilder) {
             $scope.game = currentGame;                
             $scope.dtScoreboard = {};
-            $scope.dtScoreboard.dtOptions = DTOptionsBuilder.newOptions()
-                .withDOM('t')
+            $scope.dtScoreboard.options = DTOptionsBuilder.newOptions()
+                .withDOM('lfrtip')
                 .withOption('scrollY', '300px')
                 .withOption('scrollX', '100%')
                 .withOption('scrollCollapse', true)
                 .withOption('paging', false)
-                .withFixedColumns({ leftColumns: 1 });
-    
-            $scope.dtScoreboard.dtColumnDefs = [
-                DTColumnDefBuilder.newColumnDef(0),
-                DTColumnDefBuilder.newColumnDef(1),
-                DTColumnDefBuilder.newColumnDef(2),
-                DTColumnDefBuilder.newColumnDef(3),
-                DTColumnDefBuilder.newColumnDef(4),
-                DTColumnDefBuilder.newColumnDef(5),
-                DTColumnDefBuilder.newColumnDef(6),
-                DTColumnDefBuilder.newColumnDef(7),
-                DTColumnDefBuilder.newColumnDef(8)
-            ];
+                .withFixedColumns({ leftColumns: 1 })
+                .withOption('responsive', false);
     }]);
