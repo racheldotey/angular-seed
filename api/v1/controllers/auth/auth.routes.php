9deaf40949d976408ced05fb4741fac6f6dcdc57
Rachel Dotey <rachel.dotey@gmail.com>
2016-04-06 08:42:09 -0400

    Route docs

--- api/v1/controllers/auth/auth.routes.php
+++ api/v1/controllers/auth/auth.routes.php
@@ -27,22 +27,129 @@ class AuthRoutes {
             
         $app->group('/auth', $authenticateForRole('public'), function () use ($app) {
             
-            ///// 
-            ///// Authentication
-            ///// 
-
-            /* apiKey, apiToken */
-            
+            /**
+             * @api {post} /auth/authenticate Confirm api key and token pair represents an active user login session.
+             * @apiName Authenticate
+             * @apiGroup Auth
+             *
+             * @apiParam {String} apiKey User session key.
+             * @apiParam {String} apiToken User session unhashed token.
+             *
+             * @apiSuccessExample {json} Success-Response:
+             *      HTTP/1.1 200: OK
+             *      {
+             *          "data": {
+             *              "authenticated": true,
+             *              "sessionLifeHours": 1
+             *              "user": {
+             *                  "id": "28",
+             *                  "nameFirst": "Rachel",
+             *                  "nameLast": "Testing",
+             *                  "email": "racheltest@testing.com",
+             *                  "displayName": "Rachel",
+             *                  "roles": ['3'],
+             *                  "apiKey": "caf02551768a09e1aed8946ecacce3b01f253884a08bded1f1a76520b8f0c4e847914a1daea072ab957582a2c32beceacd62b5e6842f18ef2b21a3f13b16c374",
+             *                  "apiToken": "c88e7640de8f34c18d7d07d6d0a26b0d9896f188766e445bac32a44cb275ba89"
+             *              }
+             *          },
+             *          "meta": {
+             *              "error": false,
+             *              "status": 200
+             *          }
+             *      }
+             * 
+             * 
+             * @apiErrorExample {json} Error-Missing-Parameters:
+             *      HTTP/1.1 401: Unauthorized
+             *      {
+             *          "data": {
+             *              "authenticated": false,
+             *              "msg": "Unauthenticated: Invalid request. Check your parameters and try again."
+             *          },
+             *          "meta": {
+             *              "error": true,
+             *              "status": 401
+             *          }
+             *      }
+             * 
+             * 
+             * @apiErrorExample {json} Error-Incorrect-Values:
+             *      HTTP/1.1 401: Unauthorized
+             *      {
+             *          "data": {
+             *              "authenticated": false,
+             *              "msg": "Unauthenticated: No User"
+             *          },
+             *          "meta": {
+             *              "error": true,
+             *              "status": 401
+             *          }
+             *      }
+             */
             $app->post("/authenticate/", function () use ($app) {
                 AuthController::isAuthenticated($app);
             });
             
-            ///// 
-            ///// Sign Up
-            ///// 
-
-            /* email, nameFirst, nameLast, password */
             
+            /**
+             * @api {post} /auth/signup Standard user signup.
+             * @apiName Signup
+             * @apiGroup Auth
+             *
+             * @apiParam {String} email User email address.
+             * @apiParam {String} passowrd User unencrypted password.
+             * @apiParam {String} nameFirst User first name.
+             * @apiParam {String} nameLast User last name.
+             *
+             * @apiSuccessExample {json} Success-Response:
+             *      HTTP/1.1 200: OK
+             *      {
+             *          "data": {
+             *              "registered": true,
+             *              "sessionLifeHours": 1
+             *              "user": {
+             *                  "id": "28",
+             *                  "nameFirst": "Rachel",
+             *                  "nameLast": "Testing",
+             *                  "email": "racheltest@testing.com",
+             *                  "displayName": "Rachel",
+             *                  "roles": ['3'],
+             *                  "apiKey": "caf02551768a09e1aed8946ecacce3b01f253884a08bded1f1a76520b8f0c4e847914a1daea072ab957582a2c32beceacd62b5e6842f18ef2b21a3f13b16c374",
+             *                  "apiToken": "c88e7640de8f34c18d7d07d6d0a26b0d9896f188766e445bac32a44cb275ba89"
+             *              }
+             *          },
+             *          "meta": {
+             *              "error": false,
+             *              "status": 200
+             *          }
+             *      }
+             * 
+             * @apiErrorExample {json} Error-Missing-Parameters:
+             *      HTTP/1.1 400: Bad Request
+             *      {
+             *          "data": {
+             *              "registered": false,
+             *              "msg": "Signup  failed. Check your parameters and try again."
+             *          },
+             *          "meta": {
+             *              "error": true,
+             *              "status": 400
+             *          }
+             *      }
+             * 
+             * @apiErrorExample {json} Error-Duplicate-Email:
+             *      HTTP/1.1 400: Bad Request
+             *      {
+             *          "data": {
+             *              "registered": false,
+             *              "msg": "Signup failed. A user with that email already exists."
+             *          },
+             *          "meta": {
+             *              "error": true,
+             *              "status": 400
+             *          }
+             *      }
+             */
             $app->post("/signup/", function () use ($app) {
                 AuthController::signup($app);
             });
@@ -71,11 +178,77 @@ class AuthRoutes {
                 InfoController::saveAdditional($app);
             });
             
-            ///// 
-            ///// Login
-            ///// 
-            
-            /* email, passowrd */
+            /**
+             * @api {post} /auth/login Standard user login.
+             * @apiName Login
+             * @apiGroup Auth
+             *
+             * @apiParam {String} email User email address.
+             * @apiParam {String} passowrd User unencrypted password.
+             *
+             * @apiSuccessExample {json} Success-Response:
+             *      HTTP/1.1 200: OK
+             *      {
+             *          "data": {
+             *              "authenticated": true,
+             *              "sessionLifeHours": 1
+             *              "user": {
+             *                  "id": "28",
+             *                  "nameFirst": "Rachel",
+             *                  "nameLast": "Testing",
+             *                  "email": "racheltest@testing.com",
+             *                  "displayName": "Rachel",
+             *                  "roles": ['3'],
+             *                  "apiKey": "caf02551768a09e1aed8946ecacce3b01f253884a08bded1f1a76520b8f0c4e847914a1daea072ab957582a2c32beceacd62b5e6842f18ef2b21a3f13b16c374",
+             *                  "apiToken": "c88e7640de8f34c18d7d07d6d0a26b0d9896f188766e445bac32a44cb275ba89"
+             *              }
+             *          },
+             *          "meta": {
+             *              "error": false,
+             *              "status": 200
+             *          }
+             *      }
+             * 
+             * @apiErrorExample {json} Error-Missing-Parameters:
+             *      HTTP/1.1 401: Unauthorized
+             *      {
+             *          "data": {
+             *              "authenticated": false,
+             *              "msg": "Login failed. Check your parameters and try again."
+             *          },
+             *          "meta": {
+             *              "error": true,
+             *              "status": 401
+             *          }
+             *      }
+             * 
+             * @apiErrorExample {json} Error-Unregistered-Email:
+             *      HTTP/1.1 401: Unauthorized
+             *      {
+             *          "data": {
+             *              "authenticated": false,
+             *              "msg": "Unauthenticated: No User"
+             *          },
+             *          "meta": {
+             *              "error": true,
+             *              "status": 401
+             *          }
+             *      }
+             * 
+             * @apiErrorExample {json} Error-Incorrect-Password:
+             *      HTTP/1.1 401: Unauthorized
+             *      {
+             *          "data": {
+             *              "authenticated": false,
+             *              "maxattempts": 6,
+             *              "msg": "Login failed. Username and password combination did not match."
+             *          },
+             *          "meta": {
+             *              "error": true,
+             *              "status": 401
+             *          }
+             *      }
+             */
             $app->post("/login/", function () use ($app) {
                 AuthController::login($app);
             });
