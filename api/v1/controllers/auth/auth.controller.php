745775c0da9ee0e4718523204ca2d1c50560cd6a
Rachel Dotey <rachel.dotey@gmail.com>
2016-05-05 20:10:41 -0400

    formatting

--- api/v1/controllers/auth/auth.controller.php
+++ api/v1/controllers/auth/auth.controller.php
@@ -1,4 +1,7 @@
-<?php namespace API;
+<?php
+
+namespace API;
+
 require_once dirname(dirname(dirname(__FILE__))) . '/services/api.auth.php';
 require_once dirname(dirname(__FILE__)) . '/venues/venues.data.php';
 require_once dirname(__FILE__) . '/auth.data.php';
@@ -10,54 +13,56 @@ require_once dirname(__FILE__) . '/auth.hooks.php';
 use \Respect\Validation\Validator as v;
 
 class AuthController {
-    
+
     ///// 
     ///// Authentication
     ///// 
-            
+
     /*
      * apiKey, apiToken
      */
     static function isAuthenticated($app) {
         $found = AuthControllerNative::isAuthenticated($app);
-        if($found['authenticated']) {
+        if ($found['authenticated']) {
             return $app->render(200, $found);
-        }  else {
+        } else {
             return $app->render(401, $found);
         }
     }
-            
+
     ///// 
     ///// Sign Up
     ///// 
-            
+
     /* email, nameFirst, nameLast, password */
-    
+
     static function signup($app) {
         $result = AuthControllerNative::signup($app);
-        if($result['registered']) {
+        if ($result['registered']) {
             AuthHooks::signup($app, $result);
             return $app->render(200, $result);
         } else {
             return $app->render(400, $result);
         }
     }
-    
+
     /* email, nameFirst, nameLast, facebookId, accessToken */
+
     static function facebookSignup($app) {
         $result = AuthControllerFacebook::signup($app);
-        if($result['registered']) {
+        if ($result['registered']) {
             AuthHooks::signup($app, $result);
             return $app->render(200, $result);
         } else {
             return $app->render(400, $result);
         }
     }
-    
+
     /* email, nameFirst, nameLast, password */
+
     static function venueSignup($app) {
         $result = AuthControllerNative::signup($app);
-        if(!$result['registered']) {
+        if (!$result['registered']) {
             return $app->render(400, $result);
         }
         $venue = self::addVenue($app, $result['user']->id);
@@ -68,124 +73,124 @@ class AuthController {
         self::addVenueRole($result['user']->id, $venue, 'owner');
         return $app->render(200, $result);
     }
-    
+
     static function venueFacebookSignup($app) {
         $result = AuthControllerFacebook::signup($app);
-        if(!$result['registered']) {
+        if (!$result['registered']) {
             return $app->render(400, $result);
         }
         $venue = self::addVenue($app, $result['user']->id);
-        if(!$venue) {
+        if (!$venue) {
             return $app->render(400, "Could not add venue. Check your parameters and try again.");
         }
         self::addVenueGroupToUser($result['user']->id);
         self::addVenueRole($result['user']->id, $venue, 'owner');
         return $app->render(200, $result);
     }
-    
+
     static function addVenue($app, $userId) {
         $post = $app->request->post();
-        
-        if(!v::key('venueName', v::stringType())->validate($post) || 
-           !v::key('address', v::stringType())->validate($post) || 
-           !v::key('city', v::stringType())->validate($post) || 
-           !v::key('state', v::stringType())->validate($post) || 
-           !v::key('zip', v::stringType())->validate($post)) {
+
+        if (!v::key('venueName', v::stringType())->validate($post) ||
+                !v::key('address', v::stringType())->validate($post) ||
+                !v::key('city', v::stringType())->validate($post) ||
+                !v::key('state', v::stringType())->validate($post) ||
+                !v::key('zip', v::stringType())->validate($post)) {
             return false;
         }
-        
+
         $venue = array(
-            ':name' => $post['venueName'], 
-            ':address' => $post['address'], 
-            ':address_b' => (v::key('addressb', v::stringType())->validate($post)) ? $post['addressb'] : '', 
-            ':city' => $post['city'], 
-            ':state' => $post['state'], 
-            ':zip' => $post['zip'], 
-            ':phone' => (v::key('phone', v::stringType())->validate($post)) ? $post['phone'] : '', 
-            ':website' => (v::key('website', v::stringType())->validate($post)) ? $post['website'] : '', 
-            ':facebook_url' => (v::key('facebook', v::stringType())->validate($post)) ? $post['facebook'] : '', 
-            ':logo' => (v::key('logo', v::stringType())->validate($post)) ? $post['logo'] : '', 
-            ':hours' => (v::key('hours', v::stringType())->validate($post)) ? $post['hours'] : '', 
-            ':referral' => (v::key('referralCode', v::stringType())->validate($post)) ? $post['referralCode'] : '', 
+            ':name' => $post['venueName'],
+            ':address' => $post['address'],
+            ':address_b' => (v::key('addressb', v::stringType())->validate($post)) ? $post['addressb'] : '',
+            ':city' => $post['city'],
+            ':state' => $post['state'],
+            ':zip' => $post['zip'],
+            ':phone' => (v::key('phone', v::stringType())->validate($post)) ? $post['phone'] : '',
+            ':website' => (v::key('website', v::stringType())->validate($post)) ? $post['website'] : '',
+            ':facebook_url' => (v::key('facebook', v::stringType())->validate($post)) ? $post['facebook'] : '',
+            ':logo' => (v::key('logo', v::stringType())->validate($post)) ? $post['logo'] : '',
+            ':hours' => (v::key('hours', v::stringType())->validate($post)) ? $post['hours'] : '',
+            ':referral' => (v::key('referralCode', v::stringType())->validate($post)) ? $post['referralCode'] : '',
             ":created_user_id" => $userId,
             ":last_updated_by" => $userId
         );
         return VenueData::insertVenue($venue);
     }
-    
+
     static function addVenueGroupToUser($userId) {
         $groupId = GroupData::selectGroupIdBySlug('venue-editor');
-        if(!$groupId) {
+        if (!$groupId) {
             return false;
         }
         return UserData::insertGroupAssignment(array(
-            ':auth_group_id' => $groupId, 
-            ':user_id' => $userId,
-            ':created_user_id' => $userId
+                    ':auth_group_id' => $groupId,
+                    ':user_id' => $userId,
+                    ':created_user_id' => $userId
         ));
     }
-    
+
     static function addVenueRole($userId, $venueId, $roleSlug) {
         $role = ($roleSlug === 'owner' || $roleSlug === 'manager' || $roleSlug === 'employee' || $roleSlug === 'guest');
         return VenueData::insertVenueRoleAssignment(array(
-            ':venue_id' => $venueId, 
-            ':user_id' => $userId,
-            ':role' => $role
+                    ':venue_id' => $venueId,
+                    ':user_id' => $userId,
+                    ':role' => $role
         ));
     }
 
     ///// 
     ///// Authentication
     ///// 
-              
+
     /*
      * email, password, remember
      */
     static function login($app) {
         $result = AuthControllerNative::login($app);
-        if($result['authenticated']) {
+        if ($result['authenticated']) {
             return $app->render(200, $result);
         } else {
             return $app->render(401, $result);
         }
     }
-	
-	 static function forgotpassword($app) {
+
+    static function forgotpassword($app) {
         $result = AuthControllerNative::forgotpassword($app);
-        if($result['frgtauthenticated']) {
+        if ($result['frgtauthenticated']) {
             return $app->render(200, $result);
         } else {
             return $app->render(400, $result);
         }
     }
-	
-	 static function getforgotpasswordemail($app) {
+
+    static function getforgotpasswordemail($app) {
         $result = AuthControllerNative::getforgotpasswordemail($app);
-        if($result['frgtauthenticatedemail']) {
+        if ($result['frgtauthenticatedemail']) {
             return $app->render(200, $result);
         } else {
             return $app->render(400, $result);
         }
     }
-	 static function resetpassword($app) {
+
+    static function resetpassword($app) {
         $result = AuthControllerNative::resetpassword($app);
-        if($result['resetpasswordauthenticated']) {
+        if ($result['resetpasswordauthenticated']) {
             return $app->render(200, $result);
         } else {
             return $app->render(400, $result);
         }
     }
-	
-    
+
     static function facebookLogin($app) {
         $result = AuthControllerFacebook::login($app);
-        if($result['authenticated']) {
+        if ($result['authenticated']) {
             return $app->render(200, $result);
         } else {
             return $app->render(401, $result);
         }
     }
-            
+
     ///// 
     ///// Logout
     ///// 
@@ -194,34 +199,34 @@ class AuthController {
      * logout (apiKey)
      */
     static function logout($app) {
-        if(AuthControllerNative::logout($app)) {
-            return $app->render(200, array('msg' => "User sucessfully logged out." ));
+        if (AuthControllerNative::logout($app)) {
+            return $app->render(200, array('msg' => "User sucessfully logged out."));
         } else {
-            return $app->render(400, array('msg' => "User could not be logged out. Check your parameters and try again." ));
+            return $app->render(400, array('msg' => "User could not be logged out. Check your parameters and try again."));
         }
     }
-        
+
     static function changeUserPassword($app) {
         $post = $app->request->post();
-        if((!v::key('userId', v::stringType())->validate($post) && !v::key('email', v::stringType())->validate($post)) ||
-            !v::key('current', v::stringType())->validate($post)) {
-            return $app->render(400, array('msg' => "Password could not be changed. Check your parameters and try again." ));
+        if ((!v::key('userId', v::stringType())->validate($post) && !v::key('email', v::stringType())->validate($post)) ||
+                !v::key('current', v::stringType())->validate($post)) {
+            return $app->render(400, array('msg' => "Password could not be changed. Check your parameters and try again."));
         } else if (!AuthControllerNative::validatePasswordRequirements($post, 'new')) {
             return $app->render(400, array('msg' => "Invalid Password. Check your parameters and try again."));
         }
-        
+
         $savedPassword = (v::key('userId', v::stringType())->validate($post)) ? AuthData::selectUserPasswordById($post['userId']) :
                 AuthData::selectUserPasswordByEmail($post['email']);
-        
+
         if (!$savedPassword) {
-            return $app->render(400, array('msg' => "User not found. Check your parameters and try again." ));
+            return $app->render(400, array('msg' => "User not found. Check your parameters and try again."));
         } else if (!password_verify($post['current'], $savedPassword)) {
-            return $app->render(401, array('msg' => "Invalid user password. Unable to verify request." ));
+            return $app->render(401, array('msg' => "Invalid user password. Unable to verify request."));
         } else {
-            if(AuthData::updateUserPassword(array(':id' => $post['userId'], ':password' => password_hash($post['new'], PASSWORD_DEFAULT)))) {
-                return $app->render(200, array('msg' => "Password successfully changed." ));
+            if (AuthData::updateUserPassword(array(':id' => $post['userId'], ':password' => password_hash($post['new'], PASSWORD_DEFAULT)))) {
+                return $app->render(200, array('msg' => "Password successfully changed."));
             } else {
-                return $app->render(400, array('msg' => "Password could not be changed. Try again later." ));
+                return $app->render(400, array('msg' => "Password could not be changed. Try again later."));
             }
         }
     }
@@ -230,11 +235,11 @@ class AuthController {
     // System Admin
     // TODO: Create system functions class
     ///// 
-    
     // TODO: Add this to Cron Job
     static function deleteExpiredAuthTokens($app) {
         AuthData::deleteExpiredAuthTokens();
-        return $app->render(200, array('msg' => "Deleted expired auth tokens." ));
+        return $app->render(200, array('msg' => "Deleted expired auth tokens."));
     }
-    
+
 }
+//
--- api/v1/controllers/auth/auth.hooks.php
+++ api/v1/controllers/auth/auth.hooks.php
@@ -1,30 +1,33 @@
-<?php namespace API;
+<?php
+
+namespace API;
+
 require_once dirname(__FILE__) . '/auth.data.php';
 
 class AuthHooks {
-    
+
     static function signup($app, $apiResponse) {
         self::hookCallHotSalsa($app, $apiResponse);
-    }    
-    
+    }
+
     private static function hookCallHotSalsa($app, $apiResponse) {
         $vars = self::data_hookConfigVars('HOT_SALSA_');
-        
-        if(!isset($vars['HOT_SALSA_PLAYER_REGISTRATION_ENABLED']) || ($vars['HOT_SALSA_PLAYER_REGISTRATION_ENABLED'] !== 'true' && $vars['HOT_SALSA_PLAYER_REGISTRATION_ENABLED'] !== '1')) {
+
+        if (!isset($vars['HOT_SALSA_PLAYER_REGISTRATION_ENABLED']) || ($vars['HOT_SALSA_PLAYER_REGISTRATION_ENABLED'] !== 'true' && $vars['HOT_SALSA_PLAYER_REGISTRATION_ENABLED'] !== '1')) {
             return;
         }
-        
-        if(!isset($vars['HOT_SALSA_PLAYER_REGISTRATION_URL']) || 
-            !isset($vars['HOT_SALSA_APP_VERSION']) || 
-            !isset($vars['HOT_SALSA_URL_CODE']) || 
-            !isset($vars['HOT_SALSA_AUTH_KEY']) || 
-            !isset($vars['HOT_SALSA_OS']) || 
-            !isset($vars['HOT_SALSA_PACKAGE_CODE'])) {
-            
+
+        if (!isset($vars['HOT_SALSA_PLAYER_REGISTRATION_URL']) ||
+                !isset($vars['HOT_SALSA_APP_VERSION']) ||
+                !isset($vars['HOT_SALSA_URL_CODE']) ||
+                !isset($vars['HOT_SALSA_AUTH_KEY']) ||
+                !isset($vars['HOT_SALSA_OS']) ||
+                !isset($vars['HOT_SALSA_PACKAGE_CODE'])) {
+
             self::data_logHotSalsaError($apiResponse['user']->id, "Could not attempt call. The Hot Salsa signup hook is enabled but a system variable is disabled or missing.", $vars);
             return;
         }
-        
+
         // Get Post Data
         $post = $app->request->post();
         $params = array(
@@ -38,29 +41,29 @@ class AuthHooks {
             'packageCode' => $vars['HOT_SALSA_PACKAGE_CODE']
         );
         // If it was standard signup
-        if(isset($post['password'])) {
+        if (isset($post['password'])) {
             $params['password'] = password_hash($post['password'], PASSWORD_DEFAULT);
         }
         // If it was facebook signup
-        if(isset($post['facebookId'])) {
+        if (isset($post['facebookId'])) {
             $params['facebookId'] = $post['facebookId'];
         }
-        
+
         // create curl resource 
-        $ch = curl_init(); 
+        $ch = curl_init();
 
         // set url 
-        curl_setopt($ch, CURLOPT_URL, $vars['HOT_SALSA_PLAYER_REGISTRATION_URL']); 
-        curl_setopt($ch, CURLOPT_POST, true); 
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $params); 
+        curl_setopt($ch, CURLOPT_URL, $vars['HOT_SALSA_PLAYER_REGISTRATION_URL']);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
 
         //return the transfer as a string 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
         // $output contains the output string 
         $curlOutput = curl_exec($ch);
-        
-        if(!$curlOutput) {
+
+        if (!$curlOutput) {
             // No Results = Error
             $error = (curl_error($ch)) ? curl_error($ch) : 'ERROR: No results';
             $info = (curl_getinfo($ch)) ? json_encode(curl_getinfo($ch)) : 'ERROR: No Info';
@@ -68,30 +71,29 @@ class AuthHooks {
         } else {
             // Results
             $curlResult = json_decode($curlOutput, true);
-            if(!isset($curlResult['status']) || $curlResult['status'] === 'failed') {
+            if (!isset($curlResult['status']) || $curlResult['status'] === 'failed') {
                 $error = (isset($curlResult['status'])) ? $curlResult['status'] : 'ERROR: Unknown error occured';
                 self::data_logHotSalsaError($apiResponse['user']->id, $error, $curlOutput);
             } else {
                 self::data_logHotSalsaResults($curlResult, $app, $apiResponse);
             }
-        }        
+        }
 
         // close curl resource to free up system resources 
         curl_close($ch);
-        
-    }    
-    
+    }
+
     private static function data_hookConfigVars($prefix) {
-        $varsQuery =  DBConn::executeQuery("SELECT name, `value` FROM " . DBConn::prefix() . "system_config "
-                . "WHERE name LIKE '{$prefix}%' AND disabled= 0;");
-                
+        $varsQuery = DBConn::executeQuery("SELECT name, `value` FROM " . DBConn::prefix() . "system_config "
+                        . "WHERE name LIKE '{$prefix}%' AND disabled= 0;");
+
         $vars = Array();
-        while($var = $varsQuery->fetch(\PDO::FETCH_OBJ)) {
+        while ($var = $varsQuery->fetch(\PDO::FETCH_OBJ)) {
             $vars[$var->name] = $var->value;
         }
         return $vars;
     }
-    
+
     private static function data_logHotSalsaResults($curlResult, $app, $apiResponse) {
         $logData = array(
             ':user_id' => $apiResponse['user']->id,
@@ -99,11 +101,11 @@ class AuthHooks {
             ':salsa_user_id' => (isset($curlResult['userData']) && isset($curlResult['userData']['userId'])) ? $curlResult['userData']['userId'] : NULL,
             ':salsa_user_data' => (isset($curlResult['userData'])) ? json_encode($curlResult['userData']) : NULL,
             ':salsa_error_message' => (isset($curlResult['errorMessage'])) ? $curlResult['errorMessage'] : NULL
-        );        
+        );
         return DBConn::insert("INSERT INTO " . DBConn::prefix() . "logs_hot_salsa_signup(user_id, salsa_call_status, salsa_user_id, salsa_user_data, salsa_error_message) "
-                . "VALUES (:user_id, :salsa_call_status, :salsa_user_id, :salsa_user_data, :salsa_error_message);", $logData);
+                        . "VALUES (:user_id, :salsa_call_status, :salsa_user_id, :salsa_user_data, :salsa_error_message);", $logData);
     }
-    
+
     private static function data_logHotSalsaError($userId, $errorMessage, $data) {
         $logData = array(
             ':user_id' => $userId,
@@ -112,6 +114,7 @@ class AuthHooks {
         );
 
         return DBConn::insert("INSERT INTO " . DBConn::prefix() . "logs_hot_salsa_signup(user_id, salsa_call_status, salsa_error_message) "
-                . "VALUES (:user_id, :salsa_call_status, :salsa_error_message);", $logData);
+                        . "VALUES (:user_id, :salsa_call_status, :salsa_error_message);", $logData);
     }
+
 }
