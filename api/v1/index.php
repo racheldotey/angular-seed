74978b859ed23d4bf3d73bdc87de7f9d928c98eb
Rachel Dotey <rachel.dotey@gmail.com>
2016-06-11 16:55:40 -0400

    timezone on api php

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,4 +1,6 @@
 <?php
+
+date_default_timezone_set('America/New_York');
 require_once dirname(__FILE__) . '/vendor/autoload.php';   // Composer components
 require_once dirname(__FILE__) . '/scripts/phpErrorHandling.php';  // Logging Service
 require_once dirname(__FILE__) . '/controllers/api.v1.php';
