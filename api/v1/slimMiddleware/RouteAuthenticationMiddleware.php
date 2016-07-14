99a34f471f8e7bfe8e68d2f02e1e87776276c2dd
Rachel Dotey <rachel.dotey@gmail.com>
2016-07-14 07:30:10 -0400

    Auth middleware methods.
    Still need to convert it to work with new method signature of Slim v3.

--- api/v1/slimMiddleware/RouteAuthenticationMiddleware.php
+++ api/v1/slimMiddleware/RouteAuthenticationMiddleware.php
@@ -3,6 +3,9 @@
 /* @author  Rachel L Carbone <hello@rachellcarbone.com> */
 
 class RouteAuthenticationMiddleware {
+    
+    const APISESSIONNAME = 'API_AUTHENTICATED_USER_ID';
+
     /**
      * Slim PHP Middleware to turn API Responses into a clean formatted JSON oject.
      *
@@ -27,5 +30,45 @@ class RouteAuthenticationMiddleware {
         $response->getBody()->write('AFTER');
 
         return $response;
+    }
+    
+    static function isAuthorized($app, $role = 'public') {
+        $user = self::authorizeApiToken($app);
+        if($user) {
+            // Save that user id
+            $_SESSION[self::APISESSIONNAME] = $user;
+            return true;
+        } else if(strtolower($role) === 'public') {
+            return true;
+        } else {
+            $response = array('data' => array(
+                'msg' => 'Unauthorized API Access', 
+                'sent' => $app->request->post()), 
+                'user' => $user,
+                'meta' => array('error' => true, 'status' => 401));
+            //$response = array('data' => array('msg' => 'Unauthorized API Access'), 'meta' => array('error' => true, 'status' => 401));
+            $app->halt(401, json_encode($response));
+            return false;
+        }
+    }
+    
+    private static function authorizeApiToken($app) {
+        if(!v::key('apiKey', v::stringType())->validate($app->request->post()) || 
+           !v::key('apiToken', v::stringType())->validate($app->request->post())) {
+            return false;
+        }
+        $user = AuthData::selectUserByIdentifierToken($app->request->post('apiKey'));
+        if(!$user) {
+            return "user";
+        }
+        if(!password_verify($app->request->post('apiToken'), $user->apiToken)) {
+            return "password";
+        }
+        // Go now. Be free little brother.
+        return $user->id;
+    }
+    
+    static function getUserId() {        
+        return (isset($_SESSION[self::APISESSIONNAME])) ? $_SESSION[self::APISESSIONNAME] : '0';
     }
 }
