bfb5949a88aefe3d129f2d1d0f266d2cd14768e3
Rachel Dotey <rachel.dotey@gmail.com>
2016-07-26 21:00:37 -0400

    Updated signup and login forms.

--- www/app/views/auth/signup/signup.controller.js
+++ www/app/views/auth/signup/signup.controller.js
@@ -7,13 +7,13 @@
  */
 
 angular.module('app.auth.signup', [])
-        .controller('AuthSignupCtrl', ['$scope', '$state', '$log', '$window', '$timeout', 'AuthService', 'AlertConfirmService',
-        function ($scope, $state, $log, $window, $timeout, AuthService, AlertConfirmService) {
+        .controller('AuthSignupCtrl', ['$scope', '$state', '$log', '$window', '$timeout', 'AuthService', 'AlertConfirmService', 'notifications',
+        function ($scope, $state, $log, $window, $timeout, AuthService, AlertConfirmService, notifications) {
+        
+        var passwordValidator = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9!@#$%^&*_+=-]{8,100})$/;
         
         $scope.$state = $state;
         $scope.form = {};
-        $scope.facebookAlerts = {};
-        $scope.signupAlerts = {};
         
         $scope.showPasswordRules = false;
         $scope.showPasswordMissmatch = false;
@@ -27,84 +27,95 @@ angular.module('app.auth.signup', [])
             'password' : '',
             'passwordB' : '',
             'referrer' : '',
-            'acceptTerms' : false
+            'acceptTerms' : true,
+            'acceptNewsletter' : true
+        };
+
+        var sendSignupForm = function() {
+            AuthService.signup($scope.newUser).then(function (results) {
+                $log.debug(results);
+
+                notifications.showSuccess("Signup successful! You will recieve a confirmation email shortly.");
+
+                $timeout(function () {
+                    $state.go('');
+                }, 1000);
+            }, function (error) {
+                notifications.showError(error);
+            });
+        };
+
+        var sendFacebookSignup = function() {
+            AuthService.facebookSignup().then(function (resp) {
+                $log.debug(resp);
+                $scope.newUser = resp;
+
+                notifications.showSuccess("Signup successful! You will recieve a confirmation email shortly.");
+
+                $timeout(function () {
+                    $window.top.location.href = 'http://www.angularseed.com/registration-thank-you-page/';
+                }, 1000);
+            }, function (err) {
+                notifications.showError(err);
+            });
+        };
+
+        var triggerHoneypot = function() {
+            if(angular.isDefined($scope.newUser.honeypot)) {
+                console.log('Honeypot failed.');
+                $scope.newUser = {
+                    'userGroup' : 'player',
+                    'nameFirst' : '',
+                    'nameLast' : '',
+                    'email' : '',
+                    'phone' : '',
+                    'password' : '',
+                    'passwordB' : '',
+                    'referrer' : '',
+                    'acceptTerms' : true,
+                    'acceptNewsletter' : true
+                };
+            }
         };
 
         $scope.signup = function() {
+            notifications.closeAll();
+            triggerHoneypot();
+
             if(!$scope.form.signup.$valid) {
                 $scope.form.signup.$setDirty();
-                $scope.signupAlerts.error('Please agree to our terms of service and fill in the Email, and Password fields.');
+                notifications.showError('Please agree to our Terms of Use and fill your Name, Email, and Password.');
             } else if($scope.newUser.password !== $scope.newUser.passwordB) {
                 $scope.form.signup.$setDirty();
-                $scope.signupAlerts.error('Passwords do not match.');
+                notifications.showError('Passwords do not match.');
             } else if(!$scope.newUser.acceptTerms) {
-                AlertConfirmService.confirm('Do you agree to our <a href="http://www.angularseed.com/terms-and-conditions/" target="_blank">Terms of Service</a>?', 'Terms of Service Agreement').result.then(function (resp) {
+                AlertConfirmService.confirm('Do you agree to our <a data-ui-sref="app.public.terms" target="_blank">Terms of Use</a>?', 'Terms of Use').result.then(function (resp) {
                     $scope.newUser.acceptTerms = true;
-                    AuthService.signup($scope.newUser).then(function (results) {
-                        $log.debug(results);
-
-                        $scope.signupAlerts.success("Signup successful!  Please wait for confirmation page", 'signup');
-
-                        $timeout(function () {
-                            $window.top.location.href = 'http://www.angularseed.com/registration-thank-you-page/';
-                        }, 1000);
-                    }, function (error) {
-                        $scope.signupAlerts.error(error);
-                    });
+                    sendSignupForm();
                 }, function (err) {
-                    $scope.facebookAlerts.error('Please accept the Terms of Service to signup.');
+                    notifications.showError('Please accept the Terms of Use to signup.');
                 });
             } else {
-                AuthService.signup($scope.newUser).then(function (results) {
-                    $log.debug(results);
-                    
-                    $scope.signupAlerts.success("Signup successful!  Please wait for confirmation page", 'signup');
-
-                    $timeout(function () {
-                        $window.top.location.href = 'http://www.angularseed.com/registration-thank-you-page/';
-                    }, 1000);
-                }, function (error) {
-                    $scope.signupAlerts.error(error);
-                });
+                sendSignupForm();
             }
         };
 
         $scope.facebookSignup = function() {
+            notifications.closeAll();
+            triggerHoneypot();
+            
             if(!$scope.newUser.acceptTerms) {
-                AlertConfirmService.confirm('Do you agree to our <a href="http://www.angularseed.com/terms-and-conditions/" target="_blank">Terms of Service</a>?', 'Terms of Service Agreement').result.then(function (resp) {
+                AlertConfirmService.confirm('Do you agree to our <a data-ui-sref="app.public.terms" target="_blank">Terms of Use</a>?', 'Terms of Use').result.then(function (resp) {
                     $scope.newUser.acceptTerms = true;
-                    AuthService.facebookSignup().then(function (resp) {
-                        $log.debug(resp);
-                        $scope.newUser = resp;
-                        
-                        $scope.facebookAlerts.success("Facebook signup Successful!  Please wait for confirmation page", 'terms');
-
-                        $timeout(function () {
-                            $window.top.location.href = 'http://www.angularseed.com/registration-thank-you-page/';
-                        }, 1000);
-                    }, function (err) {
-                        $scope.facebookAlerts.error(err);
-                    });
+                    sendFacebookSignup();
                 }, function (err) {
-                    $scope.facebookAlerts.error('Please accept the Terms of Service to signup.');
+                    notifications.showError('Please accept the Terms of Use to signup.');
                 });
             } else {
-                AuthService.facebookSignup().then(function (resp) {
-                    $log.debug(resp);
-                    $scope.newUser = resp;
-
-                    $scope.facebookAlerts.success("Facebook signup Successful!  Please wait for confirmation page", 'terms');
-
-                    $timeout(function () {
-                        $window.top.location.href = 'http://www.angularseed.com/registration-thank-you-page/';
-                    }, 3000);
-                }, function (err) {
-                    $scope.facebookAlerts.error(err);
-                });
+                sendFacebookSignup();
             }
         };
         
-        var passwordValidator = /^(?=.*\d)(?=.*[A-Za-z])[A-Za-z0-9_!@#$%^&*+=-]{8,55}$/;
         $scope.onChangeValidatePassword = function() {
             $scope.showPasswordRules = (!passwordValidator.test($scope.newUser.password));
             $scope.onChangeValidateConfirmPassword();
